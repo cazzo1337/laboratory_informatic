0524cf9e59d6da6b54fef6611edd9e1c995eabdd
cazzo1337 <padzhev_danil@mail.ru>
2021-03-13 09:30:08 +0500

    1st lab

---
+++ Lab_1/Lab 1/NewUserAddForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Lab_1
+{
+    partial class NewUserAddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.AddNewUser = new System.Windows.Forms.Button();
+            this.Cancelbtn = new System.Windows.Forms.Button();
+            this.dateTextBox = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(16, 49);
+            this.loginTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(212, 22);
+            this.loginTextBox.TabIndex = 0;
+            this.loginTextBox.TextChanged += new System.EventHandler(this.loginTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 95);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(16, 114);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(212, 22);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 160);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(16, 180);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(212, 22);
+            this.emailTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 160);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Телефон";
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(269, 180);
+            this.phoneTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(212, 22);
+            this.phoneTextBox.TabIndex = 6;
+            this.phoneTextBox.TextChanged += new System.EventHandler(this.phoneTextBox_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 95);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Фамилия";
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Location = new System.Drawing.Point(269, 114);
+            this.surnameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(212, 22);
+            this.surnameTextBox.TabIndex = 8;
+            this.surnameTextBox.TextChanged += new System.EventHandler(this.surnameTextBox_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(269, 30);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Имя";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(269, 49);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(212, 22);
+            this.nameTextBox.TabIndex = 10;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(527, 30);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Дата рождения";
+            // 
+            // AddNewUser
+            // 
+            this.AddNewUser.Location = new System.Drawing.Point(527, 160);
+            this.AddNewUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AddNewUser.Name = "AddNewUser";
+            this.AddNewUser.Size = new System.Drawing.Size(116, 44);
+            this.AddNewUser.TabIndex = 14;
+            this.AddNewUser.Text = "Добавить";
+            this.AddNewUser.UseVisualStyleBackColor = true;
+            this.AddNewUser.Click += new System.EventHandler(this.AddNewUser_Click);
+            // 
+            // Cancelbtn
+            // 
+            this.Cancelbtn.Location = new System.Drawing.Point(651, 160);
+            this.Cancelbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Cancelbtn.Name = "Cancelbtn";
+            this.Cancelbtn.Size = new System.Drawing.Size(116, 44);
+            this.Cancelbtn.TabIndex = 15;
+            this.Cancelbtn.Text = "Отмена";
+            this.Cancelbtn.UseVisualStyleBackColor = true;
+            this.Cancelbtn.Click += new System.EventHandler(this.Cancelbtn_Click);
+            // 
+            // dateTextBox
+            // 
+            this.dateTextBox.Location = new System.Drawing.Point(527, 49);
+            this.dateTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTextBox.Name = "dateTextBox";
+            this.dateTextBox.Size = new System.Drawing.Size(208, 22);
+            this.dateTextBox.TabIndex = 16;
+            this.dateTextBox.Value = new System.DateTime(2021, 1, 1, 0, 0, 0, 0);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(356, 144);
+            this.maskedTextBox1.Mask = "(999) 000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.maskedTextBox1.TabIndex = 17;
+            // 
+            // NewUserAddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(776, 218);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dateTextBox);
+            this.Controls.Add(this.Cancelbtn);
+            this.Controls.Add(this.AddNewUser);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "NewUserAddForm";
+            this.Text = "Новый пользователь";
+            this.Load += new System.EventHandler(this.NewUserAddForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button AddNewUser;
+        private System.Windows.Forms.Button Cancelbtn;
+        private System.Windows.Forms.DateTimePicker dateTextBox;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
---
+++ Lab_1/Lab 1/NewUserAddForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1
+{
+    public partial class NewUserAddForm : Form
+    {
+        FormUsers formUsers;
+
+        public NewUserAddForm(FormUsers formUsers)
+        {
+            InitializeComponent();
+
+            this.formUsers = formUsers;
+        }
+
+        private void NewUserAddForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddNewUser_Click(object sender, EventArgs e)
+        {
+            formUsers.AddNewUser(new FormUsers.User(passwordTextBox.Text, loginTextBox.Text, dateTextBox.Value,
+                phoneTextBox.Text, emailTextBox.Text, nameTextBox.Text, surnameTextBox.Text));
+
+            Close();
+        }
+        
+
+        private void Cancelbtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(nameTextBox.Text, "[^А-яА-Я]"))
+            {
+                MessageBox.Show("Можно использовать только русские буквы!");
+                nameTextBox.Text = Regex.Replace(nameTextBox.Text, "[^А-яА-Я]", "");
+            }
+        }
+
+        private void phoneTextBox_TextChanged(object sender, EventArgs args)
+        {
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(phoneTextBox.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Можно использовать только цифры!");
+                phoneTextBox.Text = Regex.Replace(phoneTextBox.Text, "[\\D]", ""); 
+            }
+        }
+
+        private void surnameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(surnameTextBox.Text, "[^А-яА-Я]"))
+            {
+                MessageBox.Show("Можно использовать только русские буквы!");
+                surnameTextBox.Text = Regex.Replace(surnameTextBox.Text, "[^А-яА-Я]", "");
+            }
+        }
+
+        private void loginTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(loginTextBox.Text, "[^A-zA-Z-0-9]"))
+            {
+                MessageBox.Show("Можно использовать только латинские буквы!");
+                loginTextBox.Text = Regex.Replace(loginTextBox.Text, "[^A-zA-Z-0-9]", "");
+            }
+        }
+    }
+}
